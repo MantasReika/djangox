322a7f95ba1cfb804cbbbfac2bf92c0556d7e018
balze <eilinis123@gmail.com>
2019-03-29 23:28:46 +0200

    djangox-match

--- pages/views.py
+++ pages/views.py
@@ -2,7 +2,27 @@ from django.views.generic import TemplateView
 
 
 class HomePageView(TemplateView):
-    template_name = 'pages/home.html'
+    template_name = 'pages/index.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['tournaments_list'] = self.getTournaments()
+        return context
+
+    def getTournaments(self):
+
+        tournaments = [
+            {'trName' : 'test tournament1',
+             'trTime' : '10:00:00'
+             },
+            {'trName': 'test tournament2',
+             'trTime': '12:00:00'
+             },
+            {'trName': 'test tournament3',
+             'trTime': '13:00:00'
+             }
+        ]
+        return tournaments
 
 
 class AboutPageView(TemplateView):
