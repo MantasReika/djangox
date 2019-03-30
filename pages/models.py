3f7f35c7c6bdb37d622e7b6e1aaebe52e7463b28
balze <eilinis123@gmail.com>
2019-03-30 10:53:11 +0200

    djangox-match

--- pages/models.py
+++ pages/models.py
@@ -4,23 +4,50 @@ from django.contrib.auth import get_user_model
 # Create your models here.
 
 class Hub(models.Model):
-    status = models.CharField(max_length=128, blank = True, null=True)
-    hub_name = models.CharField(max_length=128, blank = True, null=True)
-    hub_finish_dttm = models.DateTimeField()
+    UPCOMING = 'UPCOMING'
+    STARTED = 'STARTED'
+    FINISHED = 'FINISHED'
+    HUB_STA_CHOICES = (
+        (UPCOMING, 'Upcoming'),
+        (STARTED, 'STARTED'),
+        (FINISHED, 'FINISHED')
+    )
+
+    status = models.CharField(max_length=128,
+                              choices=HUB_STA_CHOICES)
+    hub_name = models.CharField(max_length=128,
+                                blank = True,
+                                null=True)
+    hub_start_dttm = models.DateTimeField(blank = True,
+                                          null=True)
+    hub_finish_dttm = models.DateTimeField(blank = True,
+                                           null=True)
 
 class HubScore(models.Model):
     hub = models.ForeignKey(Hub, on_delete=models.CASCADE)
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    place = models.IntegerField()
-    points = models.IntegerField()
+    place = models.IntegerField(blank = True,
+                                null=True)
+    points = models.IntegerField(blank = True,
+                                 null=True)
 
 class Player(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    faceit_id = models.CharField(max_length=256, blank = True, null=True)
-    eth_wallet_address = models.CharField(max_length=512, blank = True, null=True)
+    faceit_id = models.CharField(max_length=256,
+                                 blank = True,
+                                 null=True)
+    eth_wallet_address = models.CharField(max_length=512,
+                                          blank = True,
+                                          null=True)
 
 
 class Invites(models.Model):
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, blank = True, null=True)
-    invite_code = models.CharField(max_length=256)
-    transaction_hash = models.CharField(max_length=512)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE,
+                             blank = True,
+                             null=True)
+    invite_code = models.CharField(max_length=256,
+                                   blank = True,
+                                   null=True)
+    transaction_hash = models.CharField(max_length=512,
+                                        blank = True,
+                                        null=True)
